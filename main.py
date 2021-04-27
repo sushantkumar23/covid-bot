5a76db048bf6db746f799a221c0ee85a3afcde36
Sushant Kumar <sushantkumar23@gmail.com>
2021-04-27 19:03:25 +0530

    Connecting with database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
-import pymongo
 import os
+from pymongo import MongoClient
 from fastapi import FastAPI, Request
 from twilio.rest import Client
 
@@ -8,8 +8,16 @@ app = FastAPI()
 
 account_sid = os.getenv("ACCOUNT_SID")
 auth_token = os.getenv("AUTH_TOKEN")
+MONGODB_URI = os.getenv('MONGODB_URI')
+
 client = Client(account_sid, auth_token)
 
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client.get_database()
+
+leads = db['leads']
+whatsapp_requests = db['whatsapp_requests']
+whatsapp_responses = db['whatsapp_responses']
 
 @app.get("/")
 def hello_world():
@@ -18,19 +26,28 @@ def hello_world():
 
 @app.post("/incoming_message")
 async def incoming_message(request: Request):
-    print(type(request))
-    print(dir(request))
-    print(request.values())
-    print(await request.form())
-    print(await request.json())
-    incoming_msg = request.values.get('Body', '').lower()
+
+    form_data = await request.form()
+    incoming_message = dict(form_data)
+
+    whatsapp_requests.insert_one(incoming_message)
+    print(message)
     print("Incoming message: {}".format(incoming_msg))
 
+    # Reponse for Whatsapp
+    wa_response = {
+        "from": "whatsapp:+14155238886",
+        "to": "whatsapp:+919619446401",
+        "body": "The following leads are available in Mumbai for Oxygen cylinders:"
+    }
+
     message = client.messages.create(
-        from_='whatsapp:+14155238886',
-        body='The following leads are available in Mumbai for Oxygen cylinders:',
-        to='whatsapp:+919619446401'
+        from_=wa_response["from"],
+        body=wa_response["body"],
+        to=wa_response["to"]
     )
+
+    whatsapp_responses.insert_one(wa_response)
     print(message.sid)
 
 
