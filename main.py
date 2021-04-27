21130d35200cedb507e11da1d7eb2af902e37fa2
Sushant Kumar <sushantkumar23@gmail.com>
2021-04-27 17:39:22 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# main.py
+import os
+from fastapi import FastAPI, Request
+from twilio.rest import Client
+
+app = FastAPI()
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+
+@app.post("/incoming_message")
+def incoming_message(request: Request):
+    incoming_msg = request.values.get('Body', '').lower()
+
+    print("Incoming message: {}".format(incoming_msg))
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body='The following leads are available in Mumbai for Oxygen cylinders:',
+        to='whatsapp:+919619446401'
+    )
+    print(message.sid)
+
+@app.post("/status")
+def status(request):
+    request_status = request.values.get("Body", '')
+    print(request_status)
